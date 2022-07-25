2a7413cb612b980403fe06d8405306ba86500092
Atsuya Uchida <atsuya_eagle@outlook.com>
2022-07-25 21:00:14 +0900

    Added beam_search.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::io;
+use std::{
+    collections::{BinaryHeap, VecDeque},
+    io,
+};
 
 macro_rules! parse_input {
     ($x:expr, $t:ident) => {
@@ -12,7 +15,11 @@ macro_rules! parse_input {
  **/
 fn main() {
     let mut points = Point::import();
-    
+    let ans = beam_search(&points, 3);
+    for x in ans {
+        print!("{} ", x);
+    }
+    println!();
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -46,4 +53,52 @@ impl Point {
         }
         points
     }
+
+    fn length(points: &Vec<Point>, path: &Vec<usize>) -> i32 {
+        let mut length = 0.0;
+        for i in 0..path.len() - 1 {
+            length += points[path[i]].distance(&points[path[i + 1]]);
+        }
+        length as i32
+    }
+}
+
+fn beam_search(points: &Vec<Point>, beam_width: usize) -> Vec<usize> {
+    let mut deq = VecDeque::new();
+    deq.push_back((points[0], vec![0]));
+
+    let mut ans_heap = BinaryHeap::new();
+
+    loop {
+        if deq.is_empty() {
+            break;
+        }
+
+        let (point, path) = deq.pop_front().unwrap();
+        let mut heap = BinaryHeap::new();
+        for i in 0..points.len() {
+            if path.contains(&i) {
+                continue;
+            }
+            heap.push((-points[i].distance(&point) as i32, i));
+        }
+        for _ in 0..beam_width {
+            if heap.is_empty() {
+                break;
+            }
+            let (dist, i) = heap.pop().unwrap();
+            let mut new_path = path.clone();
+            new_path.push(i);
+            if new_path.len() == points.len() {
+                new_path.push(0);
+                ans_heap.push((-Point::length(points, &new_path), new_path));
+            } else {
+                deq.push_back((points[i], new_path));
+            }
+        }
+    }
+
+    let ans = ans_heap.pop().unwrap();
+
+    return ans.1;
 }
